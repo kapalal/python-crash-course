1cdf0051182fe2d83cbae76facef7844466b9d04
Nick <ivankapalala@gmail.com>
2018-01-10 17:13:07 +0100

    CHapter8

---
+++ Functions-CH8/func1.py
@@ -0,0 +1,9 @@
+def display_message():
+    """displays message"""
+    print("In this chapter im learning about functions!")
+
+
+display_message()
+
+
+
---
+++ Functions-CH8/func2.py
@@ -0,0 +1,5 @@
+def favourite_book(title):
+    print("The best book ever for me is "+ title.title()+" !")
+        
+
+favourite_book("the lord of rings")
