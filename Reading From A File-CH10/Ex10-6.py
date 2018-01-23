7cf386b5794ac872d42d7faf164a690c393c64d8
Nick <ivankapalala@gmail.com>
2018-01-23 10:56:02 +0100

    "Chapter 10 Finished"

--- Reading From A File-CH10/Ex10-6.py
+++ Reading From A File-CH10/Ex10-6.py
@@ -2,4 +2,5 @@
 # When you try to convert the input to an int, you’ll get a ValueError. 
 # Write a program that prompts for two numbers. Add them together and print the result. 
 # Catch the ValueError if either input value is not a number, and print a friendly error message. 
-# Test your program by entering two numbers and then by entering some text instead of a number.
+# Test your program by entering two numbers and then by entering some text instead of a numbe
+
